b511549a51d9fb89631846cf7d92583564839d22
Tannu <chauhantannu014@gmail.com>
2026-08-18 12:39:47 +0530

    Change ServerUrl to production endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,7 +13,7 @@ import Pricing from './pages/Pricing'
 
 import { setUserData } from './redux/userSlice'
 
-export const ServerUrl = "http://localhost:8000"
+export const ServerUrl = "https://interviewiq-u9qx.onrender.com"
 
 function App() {
 
